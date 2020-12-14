36a3f8cf6c2a4cb52972dc8686f5a5506cb24ede
JS.Yang(楊忠山) <JS.Yang@lumens.com.tw>
2020-12-14 14:47:25 +0800

    add 2nd file

---
+++ CopyIndexJS2Temp.bat
@@ -0,0 +1,3 @@
+copy "D:\JS\05-Elgatro_Stream_Deck\Code\Companion2\lib\module\lumens-visca\package.json" "\\lumens-srv-00\Temp\非機密資料暫存區\JS"
+copy "D:\JS\05-Elgatro_Stream_Deck\Code\Companion2\lib\module\lumens-visca\HELP.md" "\\lumens-srv-00\Temp\非機密資料暫存區\JS"
+copy /-y "D:\JS\05-Elgatro_Stream_Deck\Code\Companion2\lib\module\lumens-visca\index.js" "\\lumens-srv-00\Temp\非機密資料暫存區\JS"
